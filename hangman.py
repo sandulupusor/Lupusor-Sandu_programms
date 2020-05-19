8c2d218a0fd23711aadcfcec2caaa8bc9b370c6f
xWoolfy99 <65601040+xWoolfy99@users.noreply.github.com>
2020-05-19 14:21:57 +0300

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,20 @@
+
+word='bitch'
+guess='_'*len(word)
+
+k=6
+
+while k>0:
+    print(guess)
+    a=input("letter only lower case ")
+    for i in range (0,len(word)):
+        if word[i]==a:
+            guess=guess[:i]+a+guess[i+1:]
+    if guess==word:
+        print("You won")
+        break
+    if a not in word:
+        k-=1   
+    print("Lives ", k)     
+if k==0:
+    print("You lost")
